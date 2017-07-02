f23efbc7138b23f32a85f3c395c66eb76d76614a
legitfrog <legitfrog@protonmail.com>
2017-07-02 11:35:41 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var unCSS = require('gulp-uncss');
+var cleanCSS = require('gulp-clean-css');
+
+gulp.task('concat', function() {
+    gulp.src(['src/assets/css/*.css'])
+      .pipe(shell(['cat src/assets/css/*.css > src/assets/css/dev/main.min.css']));
+});
+
+gulp.task('css', function() {
+    return gulp.src(['src/assets/css/dev/main.min.css'])
+      .pipe(unCSS({html: ['*.html', 'blog/*.html', 'contact/*.html', '_layouts/*.html', '_includes/*.html']}))
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(cleanCSS({level: '2'}))
+      .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/assets/css/*.css', ['concat']);
+    gulp.watch('src/assets/css/dev/*', ['css']);
+});
